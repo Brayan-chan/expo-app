de57b3027c1b12628073b16042a1ffb24755e64d
Brayan-chan <al071392@uacam.mx>
2025-10-15 22:54:03 -0600

    Agregar lógica de carga y pantalla de inicio personalizada en RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import CustomSplashScreen from "@/components/CustomSplashScreen";
+import { View } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -19,13 +21,32 @@ function RootLayoutNav() {
 
 export default function RootLayout() {
   useEffect(() => {
-    SplashScreen.hideAsync();
+    // Simular un tiempo de carga
+    const timer = setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 2000);
+
+    return () => clearTimeout(timer);
   }, []);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <CustomSplashScreen />;
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
       <LanguageProvider>
-        <GestureHandlerRootView>
+        <GestureHandlerRootView style={{ flex: 1 }}>
           <RootLayoutNav />
         </GestureHandlerRootView>
       </LanguageProvider>
